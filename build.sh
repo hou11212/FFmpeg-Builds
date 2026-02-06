9e04407c67b54502edc6ed5756e6583641f044d1
hou11212 <houbb@novastar.tech>
2026-02-06 18:32:25 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -24,7 +24,7 @@ FFMPEG_REPO="${FFMPEG_REPO_OVERRIDE:-$FFMPEG_REPO}"
 GIT_BRANCH="${GIT_BRANCH:-master}"
 GIT_BRANCH="${GIT_BRANCH_OVERRIDE:-$GIT_BRANCH}"
 
-export FF_CONFIGURE ="--disable-everything --enable-shared --disable-static --enable-gpl --enable-encoder=h264_* --enable-libx264 --enable-encoder=libx264 --enable-swscale --enable-pthreads --enable-libvpl --enable-amf --enable-encoder=aac"
+export FF_CONFIGURE="--disable-everything --enable-shared --disable-static --enable-gpl --enable-encoder=h264_* --enable-libx264 --enable-encoder=libx264 --enable-swscale --enable-pthreads --enable-libvpl --enable-amf --enable-encoder=aac"
 
 BUILD_SCRIPT="$(mktemp)"
 trap "rm -f -- '$BUILD_SCRIPT'" EXIT
