c399cd21ca59f9acf7d1bb2ce8fbe5a08c5eb4bc
hou11212 <houbb@novastar.tech>
2026-02-06 17:45:25 +0800

    Update win64-gpl-shared.sh

--- variants/win64-gpl-shared.sh
+++ variants/win64-gpl-shared.sh
@@ -2,9 +2,4 @@
 source "$(dirname "$BASH_SOURCE")"/windows-install-shared.sh
 source "$(dirname "$BASH_SOURCE")"/defaults-gpl-shared.sh
 
-ffbuild_depends() {  
-    echo base  
-    echo onevpl      # Intel QSV/MFX (VPL)  
-    echo ffnvcodec   # NVIDIA NV codec  
-    echo rpath       # 运行时路径处理  
-}
+export FF_CONFIGURE="--disable-everything --enable-shared --disable-static --enable-encoder=h264_* --enable-libx264 --enable-encoder=libx264 --enable-swscale --enable-pthreads --enable-libvpl --enable-amf --enable-encoder=aac"
