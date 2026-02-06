6ec412548b0e3c3cf3df687ed6b2439145177c46
hou11212 <houbb@novastar.tech>
2026-02-06 15:18:48 +0800

    Update win64-gpl-shared.sh

--- variants/win64-gpl-shared.sh
+++ variants/win64-gpl-shared.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 source "$(dirname "$BASH_SOURCE")"/windows-install-shared.sh
 source "$(dirname "$BASH_SOURCE")"/defaults-gpl-shared.sh
+
+ffbuild_depends() {  
+    echo base  
+    echo onevpl      # Intel QSV/MFX (VPL)  
+    echo ffnvcodec   # NVIDIA NV codec  
+    echo rpath       # 运行时路径处理  
+}
