9fee51eeb03244675dd23eccbb8ed4d4c1b52d86
hou11212 <houbb@novastar.tech>
2026-02-06 18:06:20 +0800

    Update win64-gpl-shared.sh

--- variants/win64-gpl-shared.sh
+++ variants/win64-gpl-shared.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 source "$(dirname "$BASH_SOURCE")"/windows-install-shared.sh
 source "$(dirname "$BASH_SOURCE")"/defaults-gpl-shared.sh
-
-export FF_CONFIGURE="--disable-everything --enable-shared --disable-static --enable-encoder=h264_* --enable-libx264 --enable-encoder=libx264 --enable-swscale --enable-pthreads --enable-libvpl --enable-amf --enable-encoder=aac"
